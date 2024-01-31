ad80c666a752a9b8313cd6e708718581f34c3e55
Dalton <dragonslayer62601@gmail.com>
2024-01-31 00:45:22 -0600

    Made window pop up appear and relocated opengl files to the solution folder

--- Physarum Simulation/Source.cpp
+++ Physarum Simulation/Source.cpp
@@ -2,11 +2,75 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+void framebuffer_size_callback(GLFWwindow*, int, int);
+void processInput(GLFWwindow*);
 
 int main()
 {
+	glfwInit();
+
+	//Sets the options for glfw
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	//Creates window with given size 800x600 and name LearnOpenGL
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed To Create Window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);//sets the current window to the newly created window
+
+	//Initializes GLAD to the current glfw proccess
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	//sets the rendering view port to a given dimension first 2 parameters set inital pixel to render the last 2 set the size of the rendering viewport
+	//(You can set the size to be smaller than the window if you wanted to render other things on the screen such as a UI)
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);//makes it so when the window is resized so is the viewport
+
+	
+	while (!glfwWindowShouldClose(window))//Continues to render image on screen until window is closed
+	{
+		//Input
+		processInput(window);
+
+		//Render
+
+
+		//check and call events and swap buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
 
 
 
+
+
+	glfwTerminate();//cleans out all of the resources we allocated for glfw
 	return 0;
 }
+
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)//if escape key is pressed close the window
+		glfwSetWindowShouldClose(window, true);
+}
+
+
+
